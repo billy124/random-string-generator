9932a7832d5d46ac0c86b7bd28aaea9ccd7b4467
Billy Mahmood <billy124@msn.com>
2015-10-13 22:30:22 +0100

    added the class to generate a random string

---
+++ magmamedia/RandomStringGenerator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace magmamedia;
+
+class RandomStringGenerator {
+    const CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    public function getString($length = 50){
+        $characters = self::CHARACTERS;
+        
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        
+        return $randomString;
+    }
+    
+}
